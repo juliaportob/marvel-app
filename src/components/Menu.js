e6739733926aebee8f0ac7272586955b29f41f75
Júlia Pôrto <juportob@gmail.com>
2021-04-12 11:37:49 -0300

    feat: add css

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,30 +2,30 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Menu = () => (
-  <div>
+  <div className="main-div-menu">
     <Link to="/profile">
-      <div>
-        <p>Profile</p>
+      <div className="indiv-menu">
+        <p>Profile -</p>
       </div>
     </Link>
     <Link to="/favorite">
-      <div>
-        <p>Favorite</p>
+      <div className="indiv-menu">
+        <p>Favorite -</p>
       </div>
     </Link>
     <Link to="/characters">
-      <div>
-        <p>Characters</p>
+      <div className="indiv-menu">
+        <p>Characters -</p>
       </div>
     </Link>
     <Link to="/comics">
-      <div>
-        <p>Comics</p>
+      <div className="indiv-menu">
+        <p>Comics -</p>
       </div>
     </Link>
     <Link to="/">
-      <div onClick={() => localStorage.clear()}>
-        <p>Log out</p>
+      <div className="indiv-menu" onClick={() => localStorage.clear()}>
+        <p>Log out </p>
       </div>
     </Link>
   </div>
