8b7b49f1336a9c660d409a78b33b85d7787a04da
Júlia Pôrto <juportob@gmail.com>
2021-04-13 14:05:43 -0300

    feat: change base url

--- src/service/NativeAPI.js
+++ src/service/NativeAPI.js
@@ -1,4 +1,4 @@
-const baseURL = "http://localhost:3001/";
+const baseURL = "https://marvel-back-jp.herokuapp.com/";
 const applicationJsonContent = "application/json";
 
 export const getCharacterByName = async (name) => {
